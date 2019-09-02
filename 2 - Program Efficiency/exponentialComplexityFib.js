1b4c0beaff3173d5ee96b597111c245307ba5c54
Hillsie <andrew@iautomate.cloud>
2019-09-02 13:37:50 +1000

    Added Newton-Rapson example

---
+++ 2 - Program Efficiency/exponentialComplexityFib.js
@@ -0,0 +1,17 @@
+// Example of exponential complexity algorithm 
+// Could shortcut and say that  when a double recursion is made, this an example of exponential complexity.
+function fibonacci(n) {
+    if (n === 0) {
+        return 0;
+    } else if (n === 1) {
+        return 1;
+    } else {
+        return (fibonacci(n - 2) + fibonacci(n - 1));
+    }
+}
+
+
+console.log(fibonacci(8));
+console.log(fibonacci(9));
+console.log(fibonacci(10));
+console.log(fibonacci(39)); // takes a bit of time ... this is where to use memoization.
---
+++ 3 - Algorithms/newtonRapsonSquareR.js
@@ -0,0 +1,33 @@
+// Newton-Raphson will apply to any polynomial
+// Following example is academic and used to find square root of a number
+// I haven't yet figured out how to adapt this to finding an item in a list
+// General appproximation algorithm to find roots of a polynomial in one variable
+// Approximation to the root  is g - p(g)/p'(g)
+// simple case cx^2 + k; first derivative 2x or given g for a root, a guess g - (g^2 - k) / 2g
+
+// So lets day you wanted to find the square root of 24
+
+function newtonRaphson(n) {
+    console.log(`what's the square root of ${n}`);
+    const EPSILON = 0.01;
+    let numberOfGuesses = 0;
+    let guessedNumber = n / 2;
+
+    while (Math.abs((guessedNumber * guessedNumber) - n) >= EPSILON) {
+
+        numberOfGuesses++;
+
+        guessedNumber = guessedNumber - (((guessedNumber ** 2) - n) / (2 * guessedNumber));
+        console.log(`${numberOfGuesses++}. Guessed Number: ${guessedNumber} `);
+    }
+
+    console.log(` ****** Found your square root in  ${numberOfGuesses++} turns. The closest n is ${guessedNumber} with rounding the n is ${guessedNumber.toFixed(0)}`);
+
+    return guessedNumber;
+};
+// Couple of test cases
+console.log(newtonRaphson(24));
+console.log(newtonRaphson(500));
+console.log(newtonRaphson(787));
+console.log(newtonRaphson(44));
+console.log(newtonRaphson(1000));
